ce64cde62557118d658b3e66d5c21711c680678d
marcelo-vp <marcelos.mail@gmail.com>
2018-05-21 02:53:58 -0300

    Handling events

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -4,8 +4,10 @@ class Clock extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            date: new Date()
+            date: new Date(),
+            timeZone: "Brasilia-DF"
         };
+        this.showTimeZone = this.showTimeZone.bind(this);
     }
     componentDidMount() {
         this.timerId = setInterval(
@@ -21,11 +23,19 @@ class Clock extends React.Component {
             date: new Date()
         });
     }
+    logElemTxt(e) {
+        console.log(e.target.textContent);
+    }
+    showTimeZone() {
+        document.querySelector('[data-js="time-zone"]').textContent = this.state.timeZone;
+    }
     render() {
         return (
             <div>
-                <h1>Hello World!</h1>
+                <h1 onClick={this.logElemTxt}>Hello World!</h1>
                 <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+                <h1 onClick={this.showTimeZone}>Click here to show time zone</h1>
+                <p>The clock's time zone is <span data-js="time-zone"></span></p>
             </div>
         );
     }
